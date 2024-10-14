d17d47a17ec4e3b69c1e58f2addf49958a615f37
Virgil <virgil.crisan@student.upt.ro>
2024-10-14 21:03:04 +0300

    create mock board

--- ConcurentServer/src/Classes/Board.java
+++ ConcurentServer/src/Classes/Board.java
@@ -1,9 +1,47 @@
 package Classes;
+
 import java.util.Dictionary;
+import java.lang.Math;
+
+import Enums.TileType;
 
 public class Board {
-    Dictionary<Integer, Integer> tiles;
+    TileType[] tile_layout;
+    int[] number_layout;
     int[] ports[];
     int robber;
-    int[] numbers[];
+
+    public Board() {
+        this.initMockBoard();
+    }
+
+    private void initMockBoard() {
+        this.robber = -1;
+        this.tile_layout = new TileType[]{
+                TileType.WOOD,
+                TileType.WOOD,
+                TileType.WOOD,
+                TileType.WOOD,
+                TileType.CLAY,
+                TileType.CLAY,
+                TileType.CLAY,
+                TileType.STONE,
+                TileType.STONE,
+                TileType.STONE,
+                TileType.SHEEP,
+                TileType.SHEEP,
+                TileType.SHEEP,
+                TileType.WHEAT,
+                TileType.WHEAT,
+                TileType.WHEAT,
+                TileType.WHEAT,
+                TileType.SHEEP,
+                TileType.SAND
+        };
+
+        this.number_layout = new int[]{
+            2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12
+        };
+
+    }
 }
