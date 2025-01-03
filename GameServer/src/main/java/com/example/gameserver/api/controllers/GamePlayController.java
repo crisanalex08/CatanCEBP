4d992337de49ab336dae907cc1bef1fbf9c2248e
RaulBus <raul.bus@student.upt.ro>
2025-01-03 19:59:55 +0200

    Refactor DTOs and Repository by removing unused imports
    - Cleaned up GameCreateRequest and UserCreateDTO by removing unnecessary imports.
    - Removed unused Optional import from GameRepository to streamline code.
    - These changes enhance code readability and maintainability.

--- GameServer/src/main/java/com/example/gameserver/api/controllers/GamePlayController.java
+++ GameServer/src/main/java/com/example/gameserver/api/controllers/GamePlayController.java
@@ -24,7 +24,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.TextMessage;
 
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @CrossOrigin
